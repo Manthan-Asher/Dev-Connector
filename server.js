8e360b139d7cde2217a8aea652ed9f0094a601a5
Manthan <manthan.asher@gmail.com>
2020-07-19 22:28:06 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser=require('body-parser')
+const bodyParser = require("body-parser");
+const path = require("path");
+const config = require("config");
+const db = config.get("mongoURL");
 
 // Connect Database
-mongoose.connect("mongodb://localhost:27017/devconnectorDB", {
+mongoose.connect(db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex:true,
-  useFindAndModify:false
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 const app = express();
-app.use(bodyParser.json({extended:true}))
-
-app.get("/", (req, res) => res.send("API running"));
+app.use(bodyParser.json({extended: true}));
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
